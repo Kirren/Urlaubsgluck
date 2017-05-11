c73d12446ba734fe947525bb9025cbaf8e5a65d5
Kirren Wright <kirren@live.com>
2017-05-11 16:20:43 +0300

    Add functionality
    Added modal windows for login and registering, buttons anf forms interactivity, and general improvements.

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,16 @@
-// Third party
+$(function(){
+  $('.idea-container').hover(
+    function() {
+      //$( this ).children( ".idea-preview" ).css("background-blend-mode", "initial");
+      $( this ).children( ".idea-preview" ).find("div").animate({
+        bottom: "15px"
+      }, 500)
 
-//= ../../bower_components/jquery/dist/jquery.js
-//= ../../bower_components/bootstrap-sass/assets/javascripts/bootstrap.min.js
+    }, function() {
+      //$( this ).children( ".idea-preview" ).css("background-blend-mode", "darken");
+      $( this ).children( ".idea-preview" ).find("div").animate({
+        bottom: "-30px"
+      }, 500)
+    }
+  );
+});
