e6f04160b28a9f1f4183948466786d54d8f4f01b
Kirren Wright <kirren@live.com>
2017-05-16 12:35:27 +0300

    Add compiled result

---
+++ public/js/main.js
@@ -0,0 +1,2 @@
+$(function(){$(".idea-container").hover(function(){$(this).children(".idea-preview").find("div").animate({bottom:"15px"},500)},function(){$(this).children(".idea-preview").find("div").animate({bottom:"-30px"},500)})});
+//# sourceMappingURL=maps/main.js.map
